473e39d35b319e22fe2f83d746c678a3b3a30c46
LuchoNoPrograma <105525189+LuchoNoPrograma@users.noreply.github.com>
2022-09-10 23:53:14 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hola StackBlitz!</h1>
+      <p>Estoy aprendiendo a editar en IDE Online, es lo maximo!! :)</p>
+    </div>
+  );
+}
